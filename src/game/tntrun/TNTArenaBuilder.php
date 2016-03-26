9c6c573ba9adcf2cf170114ca2846cdd7943f464
DavidJBrockway <twitter@pocketserver.co.uk>
2016-03-26 10:55:06 +0000

    Update TNTArenaBuilder.php

--- src/game/tntrun/TNTArenaBuilder.php
+++ src/game/tntrun/TNTArenaBuilder.php
@@ -151,6 +151,7 @@ class TNTArenaBuilder {
 		// add middle layer
 		$this->buildBoardLayer ( $level, $px, $py + 1, $pz, 12, $size );
 		// add top layer --98
+	}
 		$this->buildBoardLayer ( $level, $px, $py + 2, $pz, 44, $size );
 	}
 	
